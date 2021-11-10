83579568a9adfa22cbeb50ec3661737fe15a236f
Cristian Prochnow <cristianprochnow@gmail.com>
2021-11-09 21:31:34 -0300

    feat: finish login development

--- lib/shared/auth/auth_controller.dart
+++ lib/shared/auth/auth_controller.dart
@@ -3,21 +3,16 @@ import 'package:payflow/shared/models/user_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthController {
-  bool _isAuthenticated = false;
   UserModel? _user;
 
-  bool get isAuthenticated => _isAuthenticated;
   UserModel get user => _user!;
 
   void setUser(BuildContext context, UserModel? user) {
     if (user != null) {
       _user = user;
-      _isAuthenticated = true;
 
       Navigator.pushReplacementNamed(context, "/home");
     } else {
-      _isAuthenticated = false;
-
       Navigator.pushReplacementNamed(context, "/login");
     }
   }
